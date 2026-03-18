a4a0d531a56560da998fe1bb7374e75dac59b1cf
Ponchopadillatapia <alfonsopadilla391@gmail.com>
2026-03-18 16:20:05 -0600

    Login funcionando

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,3 +25,31 @@
 
 </body>
 </html>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+    <h2>Iniciar Sesión</h2>
+
+    @if(session('error'))
+        <p style="color:red;">{{ session('error') }}</p>
+    @endif
+
+    <form method="POST" action="/login">
+        @csrf
+
+        <label>Email:</label><br>
+        <input type="email" name="email" required><br><br>
+
+        <label>Password:</label><br>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Entrar</button>
+    </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< HEAD
 Route::get('/login', function () {
     return view('login');
+=======
+Route::get('/login1', function () {
+    return view('login1');
+>>>>>>> dd5c2201f7dcda7078d2d4994e92d282b0580979
 });
